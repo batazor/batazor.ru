3cd21e38bb12fd1caaf26f1908256850765df204
batazor <batazor@users.noreply.github.com>
2023-10-06 12:37:26 +0000

    deploy: f47dc5cb08129e0b2d4076b8ad9beb629941a703

---
+++ _next/static/agEhc4p-cnZVW8XqW5O5f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/2cca2479-56f42b67a77d58e5.js","static/chunks/pages/index-c4cda925636d99a0.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
